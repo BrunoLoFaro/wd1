d10fe225d6438385e52951e142193dc3bcdcf73f
Bruno <lofarobruno@gmail.com>
2021-10-18 17:36:42 -0300

    mongoose

---
+++ models/producto.model.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+const productosCollection = 'productos'
+const productoSchema = new mongoose.Schema({
+    id:{type:Number, require: true, max:100},
+    timestamp:{type:String, require: true, max:100},
+    nombre:{type:String, require: true, max:100},
+    descripcion:{type:String, require: true, max:100},
+    codigo:{type:String, require: true, max:100},
+    foto:{type:String, require: true, max:100},
+    precio:{type:Number, require: true, max:5000},
+})
+
+export const productos = mongoose.model(productosCollection, productoSchema);
--- server.js
+++ server.js
@@ -8,17 +8,14 @@ const http = http1.Server(app)
 import { Server } from "socket.io";
 const io = new Server(http);
 import moment from 'moment'
-import {vProductos, vMensajes} from './lotes.js'
-import {optionsMensajes, optionsProductos} from './options/SQLite3.js';
-import knex from 'knex';
-
+import mongoose from 'mongoose'
+import * as model from './models/producto.model.js'
 
 CRUD();
 
 async function CRUD (){
     try {
-        
-        const URI = 'mongodb://localhost:27017/colegio';
+        const URI = 'mongodb://localhost:27017/ecommerce';
         await mongoose.connect(URI, 
             { 
               useNewUrlParser: true,
@@ -26,100 +23,41 @@ async function CRUD (){
               serverSelectionTimeoutMS: 1000
             })    
         console.log('Conectado a la base de datos...');
-
-        await Estudiante.updateOne({nombre: 'Lucas', apellido: 'Blanco'},  {dni: '333'});
-        console.log('DNI actualizado...');
-
-        await Estudiante.updateMany({}, {$set: {ingreso: false}});
-        console.log('Campo ingreso inicializado...');
-
-        await Estudiante.updateMany({curso: '1A'}, {$set: {ingreso: true}});
-        console.log('Campo ingreso actualizado...');
-
-        let estudiantes = await Estudiante.find({nota: {$gte: 4}}, {_id: 0, __v: 0});
-        console.log('Estudiantes que aprobaron:', estudiantes);
-
-        estudiantes = await Estudiante.find({ingreso: true}, {_id: 0, __v: 0});
-        console.log('Estudiantes que tienen ingreso en true:', estudiantes);
-
-        await Estudiante.deleteMany({ingreso: true});
-        console.log('Estudiantes con ingreso en true borrados');
-
-        estudiantes = await Estudiante.find({},{__v: 0});
-        console.log('Lista final de estudiantes:');
-        for (let estudiante of estudiantes){
-            let fechaCreacion = new Date(parseInt(estudiante['_id'].toString().substring(0, 8), 16) * 1000);
-            console.log(`${estudiante} -> Fecha de creación: ${fechaCreacion}`);
         }
-
-        await mongoose.connection.close();
-    }
     catch(error) {
         throw `Error: ${error}`;
     }
 }
 
-
-let knexMensajes = knex(optionsMensajes)
-let knexProductos = knex(optionsProductos)
-
 const PORT = 8080//process.env.PORT
 const server = http.listen(PORT,()=>console.log('SERVER ON '+PORT));
 
-(async ()=>{
-    await knexMensajes.schema.hasTable('mensajes')
-    .then(()=>console.log("table mensajes already exists"))
-    .catch(()=>{
-        knexMensajes.schema.createTable('mensajes', table => {
-            table.string('mail'),
-            table.string('mensaje'),
-            table.string('tiempo'),
-            table.increments('id')
-            console.log('Tabla de mensajes creada...');
-        }).then(()=>{
-            knex('mensajes').insert(vMensajes);
-            console.log('Mensajes insertados...');
-        })      
-    })
-    await knexProductos.schema.hasTable('productos')
-    .then(()=>console.log("table mensajes already exists"))
-    .catch(()=>{
-        knexMensajes.schema.createTable('productos', table => {
-            table.string('title'),
-            table.float('price'),
-            table.string('thumbnail'),
-            table.increments('id')
-            console.log('Tabla de productos creada...');
-        }).then(()=>{
-            knex('productos').insert(vProductos);
-            console.log('Mensajes insertados...');
-        })      
-    })
-})();
-
 //-----websocket triggers-----
     io.on('connection', (socket)=> {
-        knexMensajes = knex(optionsMensajes)
-        knexProductos = knex(optionsProductos)
+
         console.log(`conectado, cliente: ${socket}`)
-        knexMensajes.from('mensajes').select('*').then((mensajes_guardados)=>{
+
+        model.productos.find({}).then((mensajes_guardados)=>{
             io.sockets.emit('mensajes', mensajes_guardados);
         })
-        knexProductos.from('productos').select('*').then((productos_guardados)=>{
+        model.productos.find({}).then((productos_guardados)=>{
             io.sockets.emit('productos', productos_guardados);
-
         })
 
         socket.on('producto',data=>{
-            listaProd.setProducto(data)
-                io.sockets.emit('producto',data)
+                const productoSaveModel = new model.productos(prodEj)
+                productoSaveModel.save()
+                .then(()=>{
+                    io.sockets.emit('producto',data)
+                })
         })
 
         socket.on('nuevo-mensaje', (data)=>{
             let tiempo = moment().format('DD/MM/YYYY, HH:MM:SS a');
             data.tiempo = tiempo
-            knexMensajes('mensajes').insert(data)
-            .then (()=>{
+            const productoSaveModel = new model.productos(prodEj)
+            productoSaveModel.save()
+            .then ((mensajes_guardados)=>{
                 console.log('Fila insertada!');
                 knexMensajes.from('mensajes').select('*').then((mensajes_guardados)=>{
                     io.sockets.emit('mensajes', mensajes_guardados);
@@ -134,6 +72,7 @@ const server = http.listen(PORT,()=>console.log('SERVER ON '+PORT));
     
 //-----handlebar config-----
 server.on('error', error=>console.log('Error en servidor', error));
+
 app.use(express.static('views'));
 app.engine(
     "hbs",
@@ -143,7 +82,7 @@ app.engine(
         layoutsDir: "views/layouts",
         partialsDir: "views/partials"
     })
-);
+)
 
 app.set('views', 'views'); // especifica el directorio de vistas
 app.set('view engine', 'hbs'); // registra el motor de plantillas
@@ -152,6 +91,6 @@ app.use(express.urlencoded({extended: true}));
 
 //-----comportamiento de la pagina a los metodos http-----
 app.get('/', (req,res)=>{
-    var scripts = '/layouts/index.js';
-    res.render('main',{script: scripts});
-});
+var scripts = '/layouts/index.js';
+res.render('main',{script: scripts});
+})
