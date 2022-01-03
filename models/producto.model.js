4ffc0da5337532f39cdae3eaf348f4c1afdba502
Bruno <lofarobruno@gmail.com>
2022-01-03 18:40:34 -0300

    {

--- models/producto.model.js
+++ models/producto.model.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
 const productosCollection = 'productos'
 const productoSchema = new mongoose.Schema({
-    id:{type:Number, require: true},
-    timestamp:{type:String, require: true, max:100},
+    //timestamp:{type:String, require: true, max:100},
     nombre:{type:String, require: true, max:100},
+    precio:{type:Number, require: true, max:5000},
+    foto:{type:String, require: true, max:100},
     descripcion:{type:String, require: true, max:100},
     codigo:{type:String, require: true, max:100},
-    foto:{type:String, require: true, max:100},
-    precio:{type:Number, require: true, max:5000},
+    id:{type:Number, require: true}
 })
 
 const productos = mongoose.model(productosCollection, productoSchema);
